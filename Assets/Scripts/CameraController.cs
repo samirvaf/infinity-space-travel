cdee80433d2aede457c52b9c414301a1d7c8a55b
Samir Ferreira <samir.vaferreira@gmail.com>
2020-08-26 20:51:47 -0300

    Add Player and Camera controllers

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField]
+    PlayerController player;
+
+    Vector3 cameraInitialPosition;
+    Vector3 playerPosition;
+
+    private void Start()
+    {
+        cameraInitialPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerPosition = player.transform.position;
+        transform.position = cameraInitialPosition + playerPosition;
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    Rigidbody rb;
+
+    [SerializeField]
+    int forceInZ;
+
+    [SerializeField]
+    int forceInX;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rb.AddForce(0, 0, forceInZ * Time.fixedDeltaTime);
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            rb.AddForce(-forceInX * Time.fixedDeltaTime, 0, 0);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            rb.AddForce(forceInX * Time.fixedDeltaTime, 0, 0);
+        }
+    }
+}
